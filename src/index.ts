f7ff32f53d0e4d1ac61a0b88610d00b3086a7b9b
Olexandr Pascal <futupas@gmail.com>
2021-01-13 00:20:42 +0200

    translate to Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('hello world');
+import { makePseudoHtml } from './MakePseudoHtml';
+import { PseudoHTML } from './PseudoHTML';
+import { prepareString } from './PrepareEmmetString';
+// import { PseudoHTML } from './PseudoHTML';
+
+// String.prototype.toHtml = function () {
+//     return emmetToHTML(this);
+// }
+
+// To make library https://aganglada.com/blog/how-to-create-your-own-typescript-library
+
+export function emmetToHTML(emmetString: string): HTMLElement[] {
+    const preparedString = prepareString(emmetString);
+    const pseudoHtml = makePseudoHtml(preparedString);
+    // Make real html from pseudoHtml
+    // Catch errors
+    return [ new HTMLDivElement() ];
+}
+
+export function emmetToPseudoHTML(emmetString: string): PseudoHTML[] {
+    const preparedString = prepareString(emmetString);
+    const pseudoHtml = makePseudoHtml(preparedString);
+    return pseudoHtml;
+}
+
+export function appendEmmet(emmetString: string, container: HTMLElement): void {
+    const elements = emmetToHTML(emmetString);
+    elements.forEach(el => {
+        container.appendChild(el);
+    });
+}
